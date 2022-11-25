678b2ba0ec5042f326e26b58df4e20b8cf8857b9
Faisal ahmed <faisal2410@yahoo.com>
2022-11-25 20:12:17 +0600

    initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const helmet = require('helmet');
+const mongoose = require("mongoose");
+require("dotenv").config();
+const morgan = require("morgan");
+const cors = require('cors');
+const { readdirSync } = require("fs")
+
+
+// middlewares
+app.use(helmet())
+app.use(express.static('public'));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+app.use(cors());
+
+
+// DB Connection
+mongoose
+    .connect(process.env.DATABASE)
+    .then(() => console.log("DB connected"))
+    .catch((err) => console.log("DB Error => ", err));
+
+// routes middleware
+// readdirSync("./routes").map(r => app.use("/api/v1", require(`./routes/${r}`)))
+
+// server
+const port = process.env.PORT || 8000;
+
+
+app.listen(port, () => {
+    console.log(`App is running on port ${port}`);
+});
